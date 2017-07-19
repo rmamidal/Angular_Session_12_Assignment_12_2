d1643dea473d1a0a05f4180aa826a776a224722f
Rajendhar Mamidala <rajendhar.mamidala@capgemini.com>
2017-07-19 08:00:59 +0530

    Commenting

--- Routing-app/src/app/app.routes.ts
+++ Routing-app/src/app/app.routes.ts
@@ -4,9 +4,9 @@ import { AddMovieComponent } from './Component/add-movie.component'
 import { ListMovieComponent } from './Component/list-movie.component'
 import { PageNotFoundComponent } from './Component/page-not-found.component'
 
-// Defining path and assigning compinent to it.
+// Defining path and assigning component to it.
 export const ApplicationRoutes: Routes = [
-    { path:'', redirectTo:'/ListMovie', pathMatch:'full'},
+    { path:'', redirectTo:'/ListMovie', pathMatch:'full'}, /* Default routes*/
     { path:'AddMovie', component: AddMovieComponent },
     { path:'ListMovie', component: ListMovieComponent },
     { path:'**', component: PageNotFoundComponent } /* Wild card routes */
